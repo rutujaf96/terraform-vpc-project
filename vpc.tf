4b2b249c21042687a9e40187fdd6f824d5603b14
Ubuntu <ubuntu@ip-172-31-38-145.eu-north-1.compute.internal>
2026-06-30 17:19:49 +0000

    Updated IAM role and instance profile names

--- vpc.tf
+++ vpc.tf
@@ -104,7 +104,7 @@ resource "aws_security_group" "dev_sg" {
 }
 
 resource "aws_iam_role" "ec2_role" {
-  name = "Terraform-EC2-Role-1"
+  name = "Terraform-EC2-Role-2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -123,7 +123,7 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_instance_profile" "profile" {
-  name = "Terraform-Profile-1"
+  name = "Terraform-Profile-2"
   role = aws_iam_role.ec2_role.name
 }
 
