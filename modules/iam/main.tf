aab447beef58da38f2a084337d72851de0599bbe
Ubuntu <ubuntu@ip-172-31-38-145.eu-north-1.compute.internal>
2026-07-25 19:09:05 +0000

    Attach Amazon ECR read-only policy to EC2 IAM role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,37 +1,29 @@
 resource "aws_iam_role" "ec2_role" {
-
   name = "Terraform-EC2-Role-2"
 
   assume_role_policy = jsonencode({
-
     Version = "2012-10-17"
 
     Statement = [
-
       {
-
         Effect = "Allow"
 
         Principal = {
-
           Service = "ec2.amazonaws.com"
-
         }
 
         Action = "sts:AssumeRole"
-
       }
-
     ]
-
   })
+}
 
+resource "aws_iam_role_policy_attachment" "ecr_read_only" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_iam_instance_profile" "profile" {
-
   name = "Terraform-Profile-2"
-
   role = aws_iam_role.ec2_role.name
-
 }
