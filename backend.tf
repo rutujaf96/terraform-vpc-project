737e8e0e17e31e040a849336fd31ff1e1563e2d9
Ubuntu <ubuntu@ip-172-31-38-145.eu-north-1.compute.internal>
2026-07-27 19:23:52 +0000

    Add remote state and infrastructure action workflow

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "rutuja-terraform-state-560904638794"
+    key          = "terraform-vpc/dev/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
