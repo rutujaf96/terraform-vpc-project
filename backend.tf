7907ed09fbe06c9e1e4461789fcf5912bc6a10b1
Ubuntu <ubuntu@ip-172-31-38-145.eu-north-1.compute.internal>
2026-07-27 19:33:49 +0000

    Fix S3 backend compatibility with Jenkins Terraform version

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "rutuja-terraform-state-560904638794"
-    key          = "terraform-vpc/dev/terraform.tfstate"
-    region       = "eu-north-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket  = "rutuja-terraform-state-560904638794"
+    key     = "terraform-vpc/dev/terraform.tfstate"
+    region  = "eu-north-1"
+    encrypt = true
   }
 }
