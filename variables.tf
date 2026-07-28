26f1d8deca64463509db713c3f774b41327115b6
Ubuntu <ubuntu@ip-172-31-38-145.eu-north-1.compute.internal>
2026-07-28 13:54:20 +0000

    Update EC2 instance type to t3.medium for Minikube

--- variables.tf
+++ variables.tf
@@ -1,38 +1,53 @@
 variable "aws_region" {
-  default = "eu-north-1"
+  description = "AWS Region"
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet" {
   description = "CIDR block for Public Subnet"
+  type        = string
   default     = "10.0.1.0/24"
 }
 
 variable "private_subnet" {
   description = "CIDR block for Private Subnet"
+  type        = string
   default     = "10.0.2.0/24"
 }
 
 variable "availability_zone_public" {
-  default = "eu-north-1a"
+  description = "Availability Zone for Public Subnet"
+  type        = string
+  default     = "eu-north-1a"
 }
 
 variable "availability_zone_private" {
-  default = "eu-north-1b"
+  description = "Availability Zone for Private Subnet"
+  type        = string
+  default     = "eu-north-1b"
 }
 
 variable "ami_id" {
-  default = "ami-0fb18649b58075f5e"
+  description = "Ubuntu AMI ID"
+  type        = string
+  default     = "ami-0fb18649b58075f5e"
 }
 
 variable "instance_type" {
-  default = "t3.micro"
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t3.medium"
 }
 
 variable "key_name" {
-  default = "agent-key"
+  description = "AWS EC2 Key Pair"
+  type        = string
+  default     = "agent-key"
 }
